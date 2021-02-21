4065718a6a8dc6f66b3dcf2a58995466f7208c5e
Brooks <brooksyoung13@gmail.com>
2021-02-21 16:29:25 -0500

    part 2- create put mapping in backend

--- backend/restful-web-services-/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ backend/restful-web-services-/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -3,11 +3,14 @@ package com.in28minutes.rest.webservices.restfulwebservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,7 +44,6 @@ public class TodoResource
 	
 	//Delete method
 	//DELETE /users/{username}/todos/{id}
-	
 	//Response Entity helps build specific requests with specific statuses
 	@DeleteMapping("/users/{username}/todos/{id}")
 	public ResponseEntity<Void> deleteTodo(
@@ -54,4 +56,18 @@ public class TodoResource
 		}
 		return ResponseEntity.notFound().build();
 	}
+	
+	//Edit or Update a Todo
+	//PUT /users/{user_name}/todos/
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username, @PathVariable long id, @RequestBody Todo todo)
+	{
+		Todo todoUpdated = todoService.save(todo);
+		
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
+	
+	//Create a new Todo
+	//POST /users/{user_name}/todos/
 }
