45ae191e86ee39593eb1c76c9f6c1a9e7acbbe72
Brooks <brooksyoung13@gmail.com>
2021-03-12 17:47:11 -0500

    create setupAxiosInterceptors method to add authorization header to every request + fix extra slashes in TodoDataService.js

--- frontend/todo-app/src/Components/todo/AuthenticationService.js
+++ frontend/todo-app/src/Components/todo/AuthenticationService.js
@@ -1,9 +1,13 @@
+import axios from "axios";
+
 class AuthenticationService
 {
     registerSuccessfulLogin(username, password)
     {
         console.log('registerSuccessfulLogin')
         sessionStorage.setItem('authenticatedUser', username);
+        //call setup axios interceptors here
+        this.setupAxiosInterceptors()
     }
 
     logout()
@@ -24,6 +28,29 @@ class AuthenticationService
         let user = sessionStorage.getItem('authenticatedUser')
         if (user===null) return ' '
         return user
+    }
+
+    //Add authorization header to every request
+    setupAxiosInterceptors()
+    {
+        let username = 'in28minutes'
+        let password = 'dummy'
+
+        //Creating basic authentication header, btoa performs base 64 encoding
+        //Adding prefix Basic 
+        let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
+
+        //we only want to do this when user is logged in
+        axios.interceptors.request.use(
+            (config) => {
+                if(this.isUserLoggedIn())
+                {
+                config.headers.authorization = basicAuthHeader
+                }
+                return config
+            }
+            
+        )
     }
 
     
--- frontend/todo-app/src/api/todo/TodoDataService.js
+++ frontend/todo-app/src/api/todo/TodoDataService.js
@@ -8,13 +8,13 @@ class TodoDataService
         //This is returning a 'promise'
 
         //Using tics here so I Can pass in username variable
-        return axios.get(`http://localhost:8080//users/${name}/todos`);
+        return axios.get(`http://localhost:8080/users/${name}/todos`);
 
     }
 
     retrieveTodo(name, id)
     {
-        return axios.get(`http://localhost:8080//users/${name}/todos/${id}`)
+        return axios.get(`http://localhost:8080/users/${name}/todos/${id}`)
     }
 
     deleteTodo(name, id)
@@ -23,21 +23,21 @@ class TodoDataService
         //This is returning a 'promise'
 
         //Using tics here so I Can pass in username variable
-        return axios.delete(`http://localhost:8080//users/${name}/todos/${id}`);
+        return axios.delete(`http://localhost:8080/users/${name}/todos/${id}`);
 
     }
 
     updateTodo(name, id, todo)
     {
 
-        return axios.put(`http://localhost:8080//users/${name}/todos/${id}`, todo);
+        return axios.put(`http://localhost:8080/users/${name}/todos/${id}`, todo);
 
     }
 
     createTodo(name, todo)
     {
 
-        return axios.post(`http://localhost:8080//users/${name}/todos/`, todo);
+        return axios.post(`http://localhost:8080/users/${name}/todos/`, todo);
 
     }
 
