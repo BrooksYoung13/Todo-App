c8068c938d66acdf6b7b1bc3cc6233a465c1b7a6
Brooks <brooksyoung13@gmail.com>
2021-03-12 20:15:37 -0500

    Create a basic authentication rest service

---
+++ backend/restful-web-services-/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,22 @@
+package com.in28minutes.rest.webservices.restfulwebservices.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller - Tell spring this will handle rest requests
+@RestController
+//Telling spring is ok to talk to something on port 4200, we need this
+@CrossOrigin(origins="http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true", allowedHeaders = "*")
+
+public class BasicAuthenticationController 
+{
+
+	@GetMapping(path = "/basicauth")
+	public AuthenticationBean helloWorldBean()
+	{
+		//Below is going to return a bean saying Hello world and subbing name variable for %s
+		return new AuthenticationBean("This is hello world bean");
+	}
+}
